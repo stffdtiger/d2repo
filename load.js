7054fc507ff6969498b53db68307dc83ec2fd967
stffdtiger <joshpratt@hotmail.com>
2019-04-17 12:25:11 -0700

    add .html and .js files for web page

---
+++ load.js
@@ -0,0 +1,8 @@
+function InitDisplay() {
+  document.getElementById("div-crafting").classList.toggle("show");
+}
+
+function ToggleInfoSections() {
+  document.getElementById("div-breakpoints").classList.toggle("show");
+  document.getElementById("div-crafting").classList.toggle("show");
+}
